b298645e7315001e9d36a8d401729de5f76b108f
Nikita <yana05042003@mail.ru>
2019-11-26 15:33:35 +0300

    My App

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,5 +1,18 @@
 import { Component } from '@angular/core';
 
+class Item{
+  purchase: string;
+  done: boolean;
+  price: number;
+   
+  constructor(purchase: string, price: number) {
+
+      this.purchase = purchase;
+      this.price = price;
+      this.done = false;
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +20,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  text: string;
+  price: number;
+  
+  items: Item[] = 
+    [
+        { purchase: "Хлеб", done: false, price: 15.9 },
+        { purchase: "Масло", done: false, price: 60 },
+        { purchase: "Картофель", done: true, price: 22.6 },
+        { purchase: "Сыр", done: false, price:310 }
+    ];
+    addItem(): void {
+         
+        if(this.text == null || this.text.trim()=="" || this.price==null)
+            return;
+        this.items.push(new Item(this.text, this.price));
+    }
 }
