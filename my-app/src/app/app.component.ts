35c463ec2c25b7112a75771b0f58daa51f5609cd
hhh <yana05042003@mail.ru>
2019-12-03 14:19:35 +0300

    Create second component

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,17 +1,5 @@
 import { Component } from '@angular/core';
-
-class Item{
-  purchase: string;
-  done: boolean;
-  price: number;
-   
-  constructor(purchase: string, price: number) {
-
-      this.purchase = purchase;
-      this.price = price;
-      this.done = false;
-  }
-}
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -20,21 +8,5 @@ class Item{
 })
 export class AppComponent {
   title = 'my-app';
-
-  text: string;
-  price: number;
-  
-  items: Item[] = 
-    [
-        { purchase: "Хлеб", done: false, price: 15.9 },
-        { purchase: "Масло", done: false, price: 60 },
-        { purchase: "Картофель", done: true, price: 22.6 },
-        { purchase: "Сыр", done: false, price:310 }
-    ];
-    addItem(): void {
-         
-        if(this.text == null || this.text.trim()=="" || this.price==null)
-            return;
-        this.items.push(new Item(this.text, this.price));
-    }
+  name="yana";
 }
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent
   ],
   imports: [
     FormsModule,
