4361b843a0a74aac403f8357b9b96b019d8a4470
hhh <yana05042003@mail.ru>
2019-12-10 15:25:14 +0300

    Children routs

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { HomeComponent } from './routing/home/home.component';
 import { AboutComponent } from './routing/about/about.component';
 import { NotFoundComponent } from './routing/not-found/not-found.component';
 import { ItemComponent } from './routing/item/item.component';
+import { ItemDetailsComponent } from './routing/item/item-details/item-details.component';
+import { ItemStartComponent } from './routing/item/item-start/item-start.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { ItemComponent } from './routing/item/item.component';
     HomeComponent,
     AboutComponent,
     NotFoundComponent,
-    ItemComponent
+    ItemComponent,
+    ItemDetailsComponent,
+    ItemStartComponent
   ],
   imports: [
     FormsModule,
