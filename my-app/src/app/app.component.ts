55d8a8ed9bc89e89ccc3d0afa1979095bf5beda3
hhh <yana05042003@mail.ru>
2019-12-10 15:01:59 +0300

    Programm nav

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,8 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent {
 name: string = "Tom";
+constructor(private router: Router) {}
+goHome() {
+  this.router.navigate([""]);
+}
 }
